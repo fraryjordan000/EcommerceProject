a067a01f953317e14146555d196d8f7d57831032
JordanMac <fraryjordan000@gmail.com>
2019-03-20 10:06:52 -0600

    added basic components for different pages, items, and purchase tickets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ItemComponent } from './shared/item/item.component';
+import { ItemInCartComponent } from './shared/item-in-cart/item-in-cart.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { CartComponent } from './cart/cart.component';
+import { DetailsComponent } from './details/details.component';
+import { SearchComponent } from './search/search.component';
+import { AccountComponent } from './account/account.component';
+import { PurchaseTicketComponent } from './shared/purchase-ticket/purchase-ticket.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemComponent,
+    ItemInCartComponent,
+    LoginComponent,
+    HomeComponent,
+    CartComponent,
+    DetailsComponent,
+    SearchComponent,
+    AccountComponent,
+    PurchaseTicketComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/shared/item-in-cart/item-in-cart.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-item-in-cart',
+  templateUrl: './item-in-cart.component.html',
+  styleUrls: ['./item-in-cart.component.scss']
+})
+export class ItemInCartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
