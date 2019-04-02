4e697a73a6747724d858e157fd7a1ed4705d3d58
JordanMac <fraryjordan000@gmail.com>
2019-04-02 09:35:56 -0600

    changed name of auth service to be more accurate with functions within

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthService } from '../auth.service';
+import { AuthService } from '../auth_db.service';
 import { Router } from '@angular/router';
 import { first } from 'rxjs/operators';
 
