d8671609548b0eb44284b48012396204b45925a9
JordanMac <fraryjordan000@gmail.com>
2019-05-06 08:30:31 -0600

    Finished details component functionality

--- src/app/shared/item-small/item-small.component.ts
+++ src/app/shared/item-small/item-small.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { AuthService } from 'src/app/auth_db.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-item-small',
@@ -6,10 +8,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./item-small.component.scss']
 })
 export class ItemSmallComponent implements OnInit {
+  
+  @Input('product') product: any;
+  private inCart: boolean = false;
 
-  constructor() { }
+  private toggling = false;
+
+  constructor(private auth: AuthService, private router: Router) {}
 
   ngOnInit() {
+    if(this.product.inCart) this.inCart = true;
   }
 
+  toggle_inCart() {
+    this.inCart = !this.inCart;
+    if(this.toggling) return;
+    this.toggling = true;
+    if(this.inCart) {
+      this.auth.addToCart(this.product, () => {
+        this.toggling = false;
+      });
+    } else {
+      this.auth.removeFromCart(this.product.id, () => {
+        this.toggling = false;
+      });
+    }
+  }
+
+  toDetails(id: number) {
+    this.router.navigate(['/reroute', 'details/'+id]);
+  }
+  
 }
